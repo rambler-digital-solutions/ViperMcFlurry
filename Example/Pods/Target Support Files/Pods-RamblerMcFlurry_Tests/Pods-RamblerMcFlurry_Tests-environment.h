130c9490d84fd038cf19e7eecaac1322b14b716d
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-08-11 15:28:54 +0300

    Исправлены проблемы с запуском тестового приложения

---
+++ Example/Pods/Target Support Files/Pods-RamblerMcFlurry_Tests/Pods-RamblerMcFlurry_Tests-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// RamblerMcFlurry/Core
+#define COCOAPODS_POD_AVAILABLE_RamblerMcFlurry_Core
+#define COCOAPODS_VERSION_MAJOR_RamblerMcFlurry_Core 0
+#define COCOAPODS_VERSION_MINOR_RamblerMcFlurry_Core 0
+#define COCOAPODS_VERSION_PATCH_RamblerMcFlurry_Core 3
+
+// RamblerMcFlurry/Testing
+#define COCOAPODS_POD_AVAILABLE_RamblerMcFlurry_Testing
+#define COCOAPODS_VERSION_MAJOR_RamblerMcFlurry_Testing 0
+#define COCOAPODS_VERSION_MINOR_RamblerMcFlurry_Testing 0
+#define COCOAPODS_VERSION_PATCH_RamblerMcFlurry_Testing 3
+
+// Typhoon
+#define COCOAPODS_POD_AVAILABLE_Typhoon
+#define COCOAPODS_VERSION_MAJOR_Typhoon 3
+#define COCOAPODS_VERSION_MINOR_Typhoon 2
+#define COCOAPODS_VERSION_PATCH_Typhoon 5
+
+// Typhoon/no-arc
+#define COCOAPODS_POD_AVAILABLE_Typhoon_no_arc
+#define COCOAPODS_VERSION_MAJOR_Typhoon_no_arc 3
+#define COCOAPODS_VERSION_MINOR_Typhoon_no_arc 2
+#define COCOAPODS_VERSION_PATCH_Typhoon_no_arc 5
+
