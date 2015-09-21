f899e398d555e48a1e120ca09ef5e53601de78a5
Andrey Zarembo-Godzyatsky <a.zarembo-godzyatsky@rambler-co.ru>
2015-09-21 17:17:53 +0300

    Renamed RamblerMcFlurry to ViperMcFlurry

--- Code/EmbeddableModules/RamblerViperEmbedModuleSegue.m
+++ Code/EmbeddableModules/RamblerViperEmbedModuleSegue.m
@@ -1,6 +1,6 @@
 //
 //  RamblerViperEmbedModuleSegue.m
-//  Rambler McFlurry
+//  VIPER McFlurry
 //
 //  Created by Andrey Zarembo-Godzyatsky on 21/08/15.
 //  Copyright (c) 2015 Rambler DS. All rights reserved.
@@ -21,14 +21,12 @@
     [self.containerView addSubview:moduleView];
     
     moduleView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    // align moduleView from the left and right
+
     [self.containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[moduleView]-0-|"
                                                                                options:0
                                                                                metrics:nil
                                                                                  views:NSDictionaryOfVariableBindings(moduleView)]];
-    
-    // align moduleView from the top and bottom
+
     [self.containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[moduleView]-0-|"
                                                                                options:0
                                                                                metrics:nil
--- Code/IntermoduleDataTransfer/UIViewController+RamblerViperModuleTransitionHandling.h
+++ Code/IntermoduleDataTransfer/UIViewController+RamblerViperModuleTransitionHandling.h
@@ -1,6 +1,6 @@
 //
 //  UIViewController+RamblerViperModuleTransitionHandling.h
-//  Rambler McFlurry
+//  VIPER McFlurry
 //
 //  Created by Andrey Zarembo-Godzyatsky on 03/08/15.
 //  Copyright (c) 2015 Rambler DS. All rights reserved.
