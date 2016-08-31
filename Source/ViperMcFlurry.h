78359558be715b077e06d479603ec67dae17f479
Novik Gleb <g.novik@rambler-co.ru>
2016-08-31 13:07:44 +0300

    Updated podspec and project structure for dynamic frameworks support.

--- Source/ViperMcFlurry.h
+++ Source/ViperMcFlurry.h
@@ -1,12 +1,12 @@
 //
-//  IntermoduleDataTransfer.h
+//  ViperMcFlurry.h
 //  ViperMcFlurry
 //
 //  Copyright (c) 2015 Rambler DS. All rights reserved.
 //
 
-#ifndef _ViperMcFlurry_IntermoduleDataTransfer_h_
-#define _ViperMcFlurry_IntermoduleDataTransfer_h_
+#ifndef _ViperMcFlurry_h_
+#define _ViperMcFlurry_h_
 
 #import "RamblerViperModuleFactoryProtocol.h"
 #import "RamblerViperModuleInput.h"
